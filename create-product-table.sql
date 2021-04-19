fe2f8abe311b514aa62d41255a4a7415e509d1ae
kfangttxku <76404008+kfangttxku@users.noreply.github.com>
2021-04-19 22:50:25 +0700

    Update create-product-table.sql

--- create-product-table.sql
+++ create-product-table.sql
@@ -4,6 +4,6 @@ CREATE TABLE product (
    product_id INT NOT NULL AUTO_INCREMENT,
    product_name VARCHAR(30) NOT NULL,
    product_price double NOT NULL,
-   product_amount INT NOT NULL,
+   product_stock INT NOT NULL,
    PRIMARY KEY (product_id)
 );
